95e9a42c06cbc37464552ad94b572939a0ec7da5
tanmaster <tanmaster95@hotmail.com>
2020-05-12 16:05:39 +0200

    initial commit

---
+++ PrivacyBanner.js
@@ -0,0 +1,24 @@
+
+
+class PrivacyBanner extends HTMLElement {
+  constructor() {
+    super();
+    this.applicationName = this.getAttribute('application-name');
+    this.policyLink = this.getAttribute('policy-link');
+    this.onAccept = this.getAttribute('on-accept');
+  }
+
+
+  connectedCallback() {
+    this.innerHTML = `Um Ihnen den bestmöglichen Service zu gewärhleisten\\n' +
+        'speichert ${this.applicationName} personenbezogene Daten.\\n' +
+        'Wenn Sie auf der Seite weitersurfen stimmen Sie\\n' +
+        'bitte der <a href="${this.policyLink}">Datenschutz-Richtlinie</a> zu.`;
+  }
+
+  disconnectedCallback() {
+
+  }
+}
+
+window.customElements.define('privacy-banner', PrivacyBanner);
