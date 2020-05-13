4a38513642c1242176b25011227ecf1067c631b3
tanmaster <tanmaster95@hotmail.com>
2020-05-13 19:02:34 +0200

    looks like its working

--- PrivacyBanner.js
+++ PrivacyBanner.js
@@ -3,21 +3,27 @@
 class PrivacyBanner extends HTMLElement {
   constructor() {
     super();
-    this.applicationName = this.getAttribute('application-name');
-    this.policyLink = this.getAttribute('policy-link');
-    this.onAccept = this.getAttribute('on-accept');
   }
-
-
   connectedCallback() {
+    const applicationName = this.getAttribute('application-name');
+    const policyLink = this.getAttribute('policy-link');
+    const onAccept = this.getAttribute('on-accept');
+    console.log(onAccept);
     this.innerHTML = `Um Ihnen den bestmöglichen Service zu gewärhleisten
-        speichert ${this.applicationName} personenbezogene Daten.
+        speichert ${applicationName} personenbezogene Daten.
         Wenn Sie auf der Seite weitersurfen stimmen Sie
-        bitte der <a href="${this.policyLink}">Datenschutz-Richtlinie</a> zu. <button>Alles Klar!</button>`;
-  }
-
-  disconnectedCallback() {
-
+        bitte der <a href="${policyLink}">Datenschutz-Richtlinie</a> zu.`;
+    const btn = document.createElement('BUTTON');
+    btn.innerText = 'Alles Klar!';
+    btn.onclick = function() {
+      this.dispatchEvent(new CustomEvent('on-accept',
+          {
+            bubbles: true,
+            cancelable: false,
+            composed: true,
+          }));
+    };
+    this.appendChild(btn);
   }
 }
 
