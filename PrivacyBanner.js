981d8557181f8971ac5bc3747a7930b0ccaa417f
Tan <tanmaster@users.noreply.github.com>
2020-05-21 15:49:36 +0200

    Update PrivacyBanner.js

--- PrivacyBanner.js
+++ PrivacyBanner.js
@@ -5,7 +5,7 @@ class PrivacyBanner extends HTMLElement {
   connectedCallback() {
     const applicationName = this.getAttribute('application-name');
     const policyLink = this.getAttribute('policy-link');
-    const onAcceptAttributeName = this.getAttribute('on-accept');
+    const onAcceptEventName = this.getAttribute('on-accept');
     this.innerHTML = `<div>In order to guarantee you the best possible service,
         ${applicationName} stores personal data.
         If you continue browsing the site, please agree to the
@@ -13,7 +13,7 @@ class PrivacyBanner extends HTMLElement {
     const btn = document.createElement('BUTTON');
     btn.innerText = 'All right!';
     btn.onclick = function() {
-      this.dispatchEvent(new CustomEvent(onAcceptAttributeName,
+      this.dispatchEvent(new CustomEvent(onAcceptEventName,
           {
             bubbles: true,
             cancelable: false,
